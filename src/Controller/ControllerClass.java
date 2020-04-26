3669ae2a2039cabaee21d494b7197a5c5a854246
iulianvraja <64288776+iulianvraja@users.noreply.github.com>
2020-04-26 07:35:04 -0700

    Update ControllerClass.java

--- src/Controller/ControllerClass.java
+++ src/Controller/ControllerClass.java
@@ -24,13 +24,9 @@ import java.util.List;
 public class ControllerClass extends DB_Product {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView home() {
-    System.out.println("salut");
-        ApplicationContext ctx=new ClassPathXmlApplicationContext("beans.xml");
-        JdbcTemplate jdbcTemplate=(JdbcTemplate)ctx.getBean("jdbcTemplate");
-        setJdbcTemplate(jdbcTemplate);
-        List<Product> p=allProducts();
+   
         ModelAndView x=new ModelAndView("index");
-        x.addObject("lista",p);
+        
         return x;
     }
 
